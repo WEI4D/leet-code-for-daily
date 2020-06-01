1927bd04bf017ab34d6341e21888debaacfb3e50
WEI4D <chen@weid.ink>
2020-06-02 00:14:25 +0800

    1.两数之和

---
+++ 1.theSumOfTwoNum/Solution.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map map = new HashMap<>();
+        
+        return new int[3];
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        System.out.print(sol.twoSum(nums, 9));
+    }
+}
